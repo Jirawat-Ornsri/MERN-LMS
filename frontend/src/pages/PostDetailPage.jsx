f616bdb16c7e1b2dd0b5581ad23995e2ec2c7a49
Jirawat-Ornsri <jirawat.ornsri@gmail.com>
2025-03-20 21:34:13 +0700

    update

--- frontend/src/pages/PostDetailPage.jsx
+++ frontend/src/pages/PostDetailPage.jsx
@@ -28,7 +28,7 @@ const PostDetailPage = () => {
   };
 
   return (
-    <div className="h-screen  p-6 mt-16">
+    <div className="min-h-screen p-6 mt-16 mb-16">
       <div className="max-w-2xl mx-auto p-4 rounded-lg shadow">
         {selectedPost ? (
           <>
