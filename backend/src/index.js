6dff52cde11cbfa2aa868c2b8ab3cf065f55d93d
Jirawat-Ornsri <jirawat.ornsri@gmail.com>
2025-03-27 14:25:11 +0700

    update cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -8,38 +8,39 @@ import courseRoute from "./routes/course.route.js";
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import commentRoute from "./routes/comment.route.js";
-import enrollRoute from './routes/enroll.route.js'
+import enrollRoute from "./routes/enroll.route.js";
 
-import { connectDB } from './lib/db.js';
-dotenv.config()
+import { connectDB } from "./lib/db.js";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-      origin: `${process.env.FRONTEND_URL}`,
-      credentials: true,
-    })
-  );
+  cors({
+    origin: `${process.env.FRONTEND_URL}`,
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"], // เพิ่ม headers ที่อนุญาต
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // ระบุ Method ที่อนุญาต
+  })
+);
 
-
-app.use("/api/auth", authRoutes)
-app.use("/api/course", courseRoute)
-app.use("/api/users", userRoute)
+app.use("/api/auth", authRoutes);
+app.use("/api/course", courseRoute);
+app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/comments", commentRoute);
-app.use("/api/post", postRoute)
-app.use("/api/enroll", enrollRoute)
+app.use("/api/post", postRoute);
+app.use("/api/enroll", enrollRoute);
 
 // api endpoints
-app.get('/',(req,res)=>{
-  res.send('API Working...');
-})
+app.get("/", (req, res) => {
+  res.send("API Working...");
+});
 
 app.listen(PORT, () => {
-    console.log("server listening on port:",PORT);
-    connectDB()
-})
+  console.log("server listening on port:", PORT);
+  connectDB();
+});
