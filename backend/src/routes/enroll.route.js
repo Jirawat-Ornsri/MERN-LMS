6ef019115c7b75cb2c7a8174bf4a199eab589d56
Jirawat-Ornsri <jirawat.ornsri@gmail.com>
2025-03-29 22:44:35 +0700

    update code

--- backend/src/routes/enroll.route.js
+++ backend/src/routes/enroll.route.js
@@ -2,9 +2,6 @@ import express from "express";
 import {
   enrollCourse,
   getEnrollments,
-  updateVideoProgress,
-  updateQuizProgress,
-  checkCourseCompletion
 } from "../controllers/enroll.controller.js";
 
 const router = express.Router();
@@ -15,13 +12,5 @@ router.post("/", enrollCourse);
 // ดึงข้อมูลคอร์สที่ผู้ใช้ลงทะเบียนเรียน
 router.get("/:user_id", getEnrollments);
 
-// อัปเดตความคืบหน้าของวิดีโอ
-router.patch("/progress/video", updateVideoProgress);
-
-// อัปเดตความคืบหน้าของแบบทดสอบ
-router.patch("/progress/quiz", updateQuizProgress);
-
-// ตรวจสอบว่านักเรียนเรียนจบคอร์สหรือยัง
-router.get("/completion/:enrollment_id", checkCourseCompletion);
 
 export default router;
